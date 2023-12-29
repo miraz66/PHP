120757078ada7d24f931cd22afeadde6bb4ddc04
miraz66 <mirajulislam0509@gmail.com>
2023-12-30 00:03:16 +0600

    array

--- operators.php
+++ operators.php
@@ -1,1 +1,18 @@
 <?php 
+/* Operators */
+
+// Arithmetic operators (+, -, *, /, %, and **)
+
+// Assignments operators (=, +=, -=, *=, /=, %=, and **=)
+
+// Strings operators (. and )
+
+// Comparison operators (==, ===,!=, !== <=, >=,, <>, <==>, ??, ?:)
+
+// Error control operators (@)
+
+// Increment operators / Decrement operators (++, --)
+
+// Logical operators (&&, ||, and, or, xor)
+
+// Bitwise operators (&, |, ^, )
