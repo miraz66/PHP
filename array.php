c622d16c37725b2f8b7b3ceadd6f287d7c76a294
miraz66 <mirajulislam0509@gmail.com>
2023-12-29 23:47:01 +0600

    array

--- array.php
+++ array.php
@@ -39,3 +39,4 @@ echo $multipleArray["php"]['versions'][1]['release7'];
 echo $multipleArray["php"]['versions'][1]['release7'];
 echo $multipleArray["php"]['versions'][1]['release7'];
 echo $multipleArray["php"]['versions'][1]['release7'];
+echo $multipleArray["php"]['versions'][1]['release7'];
