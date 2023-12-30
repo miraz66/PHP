8d8d4243da2b4de15031688bd89407dcd403ab9c
miraz66 <mirajulislam0509@gmail.com>
2023-12-30 18:53:09 +0600

    array

--- operators.php
+++ operators.php
@@ -1,11 +1,23 @@
 <?php 
 /* Operators */
+ $x = 10;
+ $y = 3;
 
 // Arithmetic operators (+, -, *, /, %, and **)
+// echo $x ** $y;
 
 // Assignments operators (=, +=, -=, *=, /=, %=, and **=)
+// $z = ($x = 20) + 10;
+// echo $z , $x;
+// $x += 10;
+// $x **= 2; // (10 * 10) = 100
+// $x /= 2; // (10 / 2) = 5
+// $x %= 3 ; // (10 % 3) = 1
+// echo $x;
 
 // Strings operators (. and )
+$x = 'Hello'
+$x = $x . 'World';
 
 // Comparison operators (==, ===,!=, !== <=, >=,, <>, <==>, ??, ?:)
 
