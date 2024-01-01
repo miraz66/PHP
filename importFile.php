08fc8e126b6f1c49af16d54c89f29657c5027f4f
miraz66 <mirajulislam0509@gmail.com>
2024-01-01 21:12:05 +0600

    Switch

--- importFile.php
+++ importFile.php
@@ -2,8 +2,25 @@
 
 // require/ require once / include/ include once
 
+// ----------------------------------------------------------------
 // include - the file not found next line code run not all error
-include 'file.php';
 
-echo "Hello World"
+// include 'file.php';
+// echo "Hello World";
+
+// ----------------------------------------------------------------
 // require - the file not found next line code run error all code
+
+// require 'file.php';
+// echo "Hello World";
+
+// ----------------------------------------------------------------
+// require_once
+
+require_once 'file.php';
+$x++; 
+echo $x . "\n";
+
+require_once 'file.php';
+$x++; 
+echo $x . "\n";
