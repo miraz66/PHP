9e75d47f63baf0586694729e6fb7b5fc919c4e2c
miraz66 <mirajulislam0509@gmail.com>
2024-01-01 23:28:31 +0600

    Switch

--- function.php
+++ function.php
@@ -2,8 +2,15 @@
 
 declare(strict_types=1);
 
-function foo():int {
-    return "foo";
+function foo(int|float &$x, int|float $y):int | float {
+    if($x % 2 === 0) {
+        $x/= 2;
+    }
+    return $x * $y;
 }
+$a = 6.0;
+$b = 7;
 
-foo();
+
+echo foo($a, $b);
+
