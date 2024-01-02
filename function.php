b53b93e7d97522eec72046c708ec08c073496e1e
miraz66 <mirajulislam0509@gmail.com>
2024-01-02 22:40:35 +0600

    function

--- function.php
+++ function.php
@@ -2,15 +2,27 @@
 
 declare(strict_types=1);
 
-function foo(int|float &$x, int|float $y):int | float {
-    if($x % 2 === 0) {
-        $x/= 2;
-    }
-    return $x * $y;
+// function foo(int|float &$x, int|float $y):int | float {
+//     if($x % 2 === 0) {
+//         $x/= 2;
+//     }elseif($x % 2 === 1) {
+//         echo "Please";
+//     }
+//     return $x * $y;
+// }
+// $a = 6.0;
+// $b = 7;
+
+
+// echo foo($a, $b) . "\n";
+
+// var_dump($a, $b);
+
+function sum(int | float $x, int | float $y , int| float ...$number):int | float {
+    return $x + $y + array_sum($number);
 }
+
 $a = 6.0;
 $b = 7;
 
-
-echo foo($a, $b);
-
+echo sum($a, $b, 10,30,400.23, 70);
