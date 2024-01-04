73e27a5b135ad6a1a65cb71a90b582554cf55dd3
miraz66 <mirajulislam0509@gmail.com>
2024-01-04 22:09:51 +0600

    array

--- error.php
+++ error.php
@@ -1,5 +1,12 @@
 <?php 
 // Error handling
 
-trigger_error('Example error', E_USER_WARNING);
-echo 1;
+// trigger_error('Example error', E_WARNING);
+// echo 1;  
+
+function errorHandler(int $type, string $msg, ?string $file = null, ?int $line = null) {
+    echo $type . ':' . $msg . 'in ' . $file . 'on line ' . $line;
+
+    exit;
+}
+
