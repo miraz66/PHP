65abcf81eeadfa82dd7f597c8753f07cc675fae6
miraz66 <mirajulislam0509@gmail.com>
2024-01-05 07:50:53 +0600

    array

--- error.php
+++ error.php
@@ -5,8 +5,12 @@
 // echo 1;  
 
 function errorHandler(int $type, string $msg, ?string $file = null, ?int $line = null) {
-    echo $type . ':' . $msg . 'in ' . $file . 'on line ' . $line;
+    echo $type . ':' . $msg . ' in ' . $file . ' on line ' . $line . ' ';
 
     exit;
 }
 
+error_reporting(E_ALL & ~E_WARNING);
+set_error_handler('errorHandler', E_ALL);
+
+echo $x;
