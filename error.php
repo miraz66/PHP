3c8acf370aec0fda985e70fbf707be939319260e
miraz66 <mirajulislam0509@gmail.com>
2024-01-05 07:51:24 +0600

    array

--- error.php
+++ error.php
@@ -16,5 +16,6 @@ set_error_handler('errorHandler', E_ALL);
 set_error_handler('errorHandler', E_ALL);
 set_error_handler('errorHandler', E_ALL);
 set_error_handler('errorHandler', E_ALL);
+set_error_handler('errorHandler', E_ALL);
 
 echo $x;
