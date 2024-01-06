022bcadf589353ef2feb0cbf6ccbc987f5eeff79
miraz66 <mirajulislam0509@gmail.com>
2024-01-06 10:33:17 +0600

    fileSystem

--- fileSystem.php
+++ fileSystem.php
@@ -5,4 +5,25 @@
 
 // var_dump(is_file($dir[2]));
 
-mkdir('miraz');
+// if(file_exists('foo.txt')) {
+//     echo filesize('foo.txt');
+
+//     file_put_contents('foo.txt', 'hello world');
+//     clearstatcache(); 
+
+//     echo filesize('foo.txt');
+// } else { echo 'file not found ';}
+
+$file = fopen('foo.txt', 'r');
+
+var_dump($file);
+
+while ($line = fgets($file) !== false) {
+    print_r($line);
+}
+
+$content = file_get_contents('foo.txt');
+
+if(file_get_contents('foo.txt') === false) {
+    echo 'File Found';
+}
